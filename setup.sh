3db199b9fa238312cefa0b2e9a727781c5b5c62c
greg <gregturnberg@gmail.com>
2023-04-18 16:34:55 -0400

    updated setup script to compile with AFL++ instrumentation for fuzzing

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ldflags="-fsanitize=address"
 
 # Download sqlite source amalgamation
 if ! which wget > /dev/null; then
-	echo "wget not found, install wget and try again"
+	echo "ERROR: wget not found, install wget and try again"
 	exit 1
 fi
 wget https://www.sqlite.org/2023/sqlite-autoconf-3410200.tar.gz
@@ -17,8 +17,14 @@ tar -x --file=sqlite-autoconf-3410200.tar.gz || {
 	exit 1
 }
 
-# Compile with debug symbols and address sanitizer
+# Check for AFL++ installation
+if ! which afl-fuzz > /dev/null; then
+	echo "ERROR: AFL++ not found, install AFL++ and try again"
+	exit 1
+fi
+
+# Compile with debug symbols, address sanitizer, and AFL++ instrumentation
 cd sqlite-autoconf-3410200
-./configure CFLAGS="$cflags" LDFLAGS="$ldflags" --enable-debug
+./configure CFLAGS="$cflags" LDFLAGS="$ldflags" --enable-debug CC=/usr/local/bin/afl-gcc CXX=/usr/local/bin/afl-g++
 make
 
